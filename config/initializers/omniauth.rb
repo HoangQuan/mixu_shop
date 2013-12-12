1595ca82fa144c07f49f339a2a4779990e8bce2f
Hoang Quan <hoang.van.quan@framgia.com>
2013-12-12 11:24:31 +0700

    fs commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+      provider :facebook, ENV['767124323302603'], ENV['8bcfc00270662cb4ff0210d713ee4978']
+end
+
+OmniAuth.config.on_failure = Proc.new { |env|
+    OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MixuShop::Application.config.secret_key_base = 'ed17d5578d161f7d2aa4a96e1f9fab69aa20935991a23b6207eb07aee257dda6e77ef13bb81307099c5f084b8dd32b146fbb23f80aef9a293a62c504d3f120a9'
