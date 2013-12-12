09cd8fd65ba1142bcc40040425136f5472782df2
Hoang Quan <hoang.van.quan@framgia.com>
2013-12-12 13:59:23 +0700

    add sessions_controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  def new
+    redirect_to '/auth/facebook'
+  end
+end
