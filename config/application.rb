de7dade03698fd82aec77c19e2ac03d854f76d04
Hoang Quan <hoang.van.quan@framgia.com>
2013-12-20 14:22:18 +0700

    update config/application.rb for heroku

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module MixuShop
   class Application < Rails::Application
     config.assets.enabled = true
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
